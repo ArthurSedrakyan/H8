1a9adbf2a8d76f812232dcb97e99fb1057a99f27
ARTUR SEDRAKYAN <arthursedrakyan98@gmail.com>
2018-04-14 21:14:23 +0400

    Parallel Loops

---
+++ Parallel Loops/Parallel Loops/ParallelLoops.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parallel_Loops
+{
+    public static class ParallelLoops
+    {
+        public static void ParallelFor(int fromInclusive, int toExclusive, Action<int> body)
+        {
+            for (int i = fromInclusive; i < toExclusive; i++)
+            {
+              int j = i;
+              Task.Run(() => body(j));
+               
+            }
+        }
+
+        public static void ParallelForEach<TSource>(IEnumerable<TSource> source, Action<TSource> body)
+        {
+            foreach (var item in source)
+            {
+                Task.Run(() => body(item));
+            }
+        }
+
+        public static  void ParallelForEachWithOptions<TSource>(IEnumerable<TSource> source, ParallelOptions parallelOptions, Action<TSource> body)
+        {
+            if (parallelOptions.MaxDegreeOfParallelism <= 0 && parallelOptions.MaxDegreeOfParallelism !=-1)
+            {
+                throw new ArgumentOutOfRangeException("Your MaxDegreeOfParallelism is smaller then can be");
+            }
+            if (parallelOptions.CancellationToken.CanBeCanceled)
+            {
+                throw new OperationCanceledException("Someone had cancel operation");
+            }
+
+            Semaphore semaphore = new Semaphore(parallelOptions.MaxDegreeOfParallelism, parallelOptions.MaxDegreeOfParallelism);
+
+            foreach (var item in source)
+            {
+                Task.Run(() =>
+                {
+                    semaphore.WaitOne();
+                    body(item);
+                    semaphore.Release();
+                },parallelOptions.CancellationToken);
+
+            }
+        }
+    }
+}
---
+++ Parallel Loops/Parallel Loops/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parallel_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ParallelLoops.ParallelFor(0, 20, x => Console.WriteLine($"num = {x}"));
+
+            List<int> list = new List<int>(25);
+            Random rnd = new Random();
+
+            for (int i = 0; i < 25; i++)
+            {
+                list.Add(rnd.Next(1, 30));
+            }
+
+            //ParallelLoops.ParallelForEach<int>(list, x => Console.WriteLine($"list {x}"));
+
+            ParallelOptions po = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = 2
+            };
+            CancellationToken ct = new CancellationToken(false);
+            po.CancellationToken = ct;
+
+
+            ParallelLoops.ParallelForEachWithOptions(list, po, x => Console.WriteLine($"list {x}dghdhd"));
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
